6749348d1b509b8695c0215c5979711602816693
Fabio Reinhold <fabioreinhold@gmail.com>
2020-07-07 16:56:30 -0300

    add carshopper with test

--- lib/web_scraping.rb
+++ lib/web_scraping.rb
@@ -1,3 +1,33 @@
 class WebScraping
-  
+  require 'httparty'
+  require 'nokogiri'
+  require 'pp'
+
+  def self.convert_price(price)
+    price.gsub('$','').gsub(',','').to_i
+  end
+
+  def self.car_shopper(price)
+    response = HTTParty.get('https://code.evgenyrahman.com/rubycourse/carlist.html')
+    parsed_html = Nokogiri::HTML(response.body)
+    
+    car_listing = parsed_html.css('.card.car')
+
+    cars = car_listing.map do |each_car|
+      {
+        make: each_car.css('.make').text,
+        year: each_car.css('.year').text,
+        price: convert_price(each_car.css('.price').text),
+        rating: each_car.css('.star.rating').attribute("data-rating").value.to_i
+      }
+    end
+
+    
+    cars.select! do |car|
+      (car[:price] > price)
+    end
+    
+    pp cars = cars.to_a
+
+  end  
 end
--- spec/web_scraping_spec.rb
+++ spec/web_scraping_spec.rb
@@ -1,5 +1,19 @@
 require 'web_scraping'
 
-describe WebScraping do
 
+describe WebScraping do
+  describe 'car shopper' do
+    context 'create a car list for cars above $30000 ' do
+      it 'returns 3 cars' do   
+        list =  WebScraping.car_shopper(30000)
+        
+        expect(list.count).to eq(3)
+      end
+    end
+    
+    context 'create a list for cars with rating above 4' do
+      it 'return 2 cars' do   
+      end
+    end
+  end
 end
